65aec95912590f690656452eb249ef757f5c6dbc
Gisele Ramos <gisele.ramos.ismart@gmail.com>
2025-03-06 11:46:48 -0300

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ function generateExercises(event) {
   let searchInput = document.querySelector("#search");
   let apiKey = "d5tab6f2acf0b7fa7e95a21d1043o46b";
   let context =
-    "You are an expert professor and love to give exercises to your students grow. Your mission is to generate around 10 questions with a unique answer, increasing the dificulty and with alternatives, separating each alternative from the questions with <br /> and the questions from another questions with a <br />. Make sure to follow the user search and let the awswers from each exercise on the final of the text inside a <strong> element. Do not write anything besides exercises.";
+    "You are an expert professor and love to give exercises to your students grow. Your mission is to generate around 5 questions with a unique answer, increasing the dificulty and with alternatives, separating each alternative from the questions with <br /> and the questions from another questions with a <br />. Make sure to follow the user search and let the awswers from each exercise on the final of the text inside a <strong> element. Do not write anything besides exercises.";
   let prompt = `User search is: Generate school exercises about ${searchInput.value}`;
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
